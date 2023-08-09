fe62a193b4f827d2862757e47704b45d776a5dc2
alexgoit <alexandrpuryaev@gmail.com>
2023-08-09 15:33:03 +0300

    fixed

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -3,3 +3,5 @@ export const selectIsAuthorized = state => state.auth.isAuthorized;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
 
 export const selectUserName = state => state.auth.user.name;
+
+export const selectUserEmail = state => state.auth.user.email;
